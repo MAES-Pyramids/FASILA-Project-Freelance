fe6c32cea621edd512cb551cbc68ae4499b7eb3c
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-01-08 19:50:12 +0200

    adding simple Rejection Handling listeners on the process stream

--- src/server.js
+++ src/server.js
@@ -1,19 +1,47 @@
 const path = require("path");
 const dotenv = require("dotenv");
 const bot = require("./utils/telegramBot");
-const { mongoConnect } = require("./utils/mongoDB");
+const { mongoConnect, mongoDisconnect } = require("./utils/mongoDB");
 //------------------Config------------------//
 dotenv.config({ path: path.join(__dirname, "..", "config.env") });
 //------------------Listener-----------------//
 const port = process.env.PORT || 3000;
 const app = require("./app");
-
+let server;
 (async function startServer() {
   await mongoConnect();
 
-  app.listen(port, () => {
+  server = app.listen(port, () => {
     console.log(
       `Server listening on port ${port} in the ${process.env.NODE_ENV} mode`
     );
   });
 })();
+//------------Rejection Handling-------------//
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", async () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+
+  try {
+    await new Promise((resolve) => {
+      // we need to roll back any ongoing transactions
+      mongoDisconnect();
+      server.close(() => {
+        console.log("💥 Server connections closed!");
+        resolve();
+      });
+    });
+    console.log("💥 Process terminated gracefully!");
+    process.exit(0);
+  } catch (err) {
+    console.error("Error during graceful shutdown:", err);
+    process.exit(1);
+  }
+});
