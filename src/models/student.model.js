1e1a2007504b56e77b4e6f47078c1206cadc4b2f
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-01-11 03:48:34 +0200

    create pre save hook for hashing password and also model instance method for comparing passwords

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,65 +1,87 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-  },
-  phone: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide phone"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-    required: [true, "Please provide gender"],
-  },
-  semester: {
-    type: Number,
-    required: [true, "Please provide semester"],
-  },
-  telegramId: {
-    type: String,
-    unique: [true, "Telegram ID already exist in another account"],
-  },
-  facultyCard: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  wallet: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Wallet",
-  },
-  faculty: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Faculty",
-    required: [true, "Please provide faculty"],
-  },
-  favoritesDoctors: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Doctor",
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
     },
-  ],
-  idStored: {
-    type: Boolean,
-    default: false,
+    phone: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide phone"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: [true, "Please provide gender"],
+    },
+    semester: {
+      type: Number,
+      required: [true, "Please provide semester"],
+    },
+    telegramId: {
+      type: String,
+      unique: [true, "Telegram ID already exist in another account"],
+    },
+    facultyCard: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    wallet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Wallet",
+    },
+    faculty: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Faculty",
+      required: [true, "Please provide faculty"],
+    },
+    favoritesDoctors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+      },
+    ],
+    idStored: {
+      type: Boolean,
+      default: false,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    suspended: {
+      type: Boolean,
+      default: false,
+    },
   },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  suspended: {
-    type: Boolean,
-    default: false,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+studentSchema.pre("save", async (next) => {
+  let student = this;
+
+  if (!student.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(process.env.SaltWorkFactor);
+  const hash = await bcrypt.hashSync(student.password, salt);
+
+  student.password = hash;
+  return next();
 });
+
+studentSchema.methods.comparePassword = async (password) => {
+  const student = this;
+  return bcrypt.compare(password, student.password).catch((e) => false);
+};
 
 const Student = mongoose.model("Student", studentSchema);
 module.exports = Student;
