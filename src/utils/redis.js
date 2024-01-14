e371987e67a034d899f2e85fe8db45607b2124ca
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-01-14 19:02:47 +0200

    setup intial redis connection and docker image

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,3 +1,28 @@
-exports.updateUserConnection = async (userId, socketId, isConnected) => {};
+const Redis = require("ioredis");
 
-exports.getUserConnection = async (userId) => {};
+// Replace 'your_redis_host' and 'your_redis_port' with the actual host and port of your Redis container
+const redisHost = process.env.redisHost;
+const redisPort = process.env.redisPort;
+
+const redis = new Redis({
+  host: redisHost,
+  port: redisPort,
+});
+
+async function updateUserConnection(userId, socketId, isConnected) {
+  const connectionInfoString = JSON.stringify({
+    socketId: socketId,
+    isConnected: isConnected,
+  });
+
+  await redis.hset("user_sockets", userId, connectionInfoString);
+}
+
+async function getUserConnection(userId) {
+  const connectionInfoString = await redis.hget("user_sockets", userId);
+
+  if (connectionInfoString) return JSON.parse(connectionInfoString);
+  else return null;
+}
+
+module.exports = { updateUserConnection, getUserConnection };
