839558f5f33f484f7c9d4b3ca7b453280ba7ea57
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-01-25 03:23:02 +0200

    return cors as same before

--- src/app.js
+++ src/app.js
@@ -36,15 +36,18 @@ const app = express();
 // Serve static content located in the "public" directory.
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-const corsOptions = {
-  origin: "https://fasila-lib-electronic.vercel.app/",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+app.use(cors());
+app.options("*", cors());
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://fasila-lib-electronic.vercel.app/",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 app.use(
   process.env.NODE_ENV === "development" ? morgan("dev") : morgan("combined")
