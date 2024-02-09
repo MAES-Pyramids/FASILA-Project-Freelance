0ec14c2b959c8916d96d1d2e67752489c0348ea2
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-02-09 15:34:40 +0200

    adding all custom messages

--- src/utils/telegramBot.js
+++ src/utils/telegramBot.js
@@ -34,14 +34,80 @@
 //     logErrorMessage(err);
 //   }
 // };
+
+// const sendResponseMessage = async (chatId, reply) => {
+//   const PublishMessage = `للدعاية و نشر الكتب يمكنك الاتصال علي رقم المكتبة
+
+//   +201029142392
+//   أو بوت
+//   @Assitant_fasilabot`;
+
+//   const SuggestMessage = `اقترح علينا شئ جديد😃
+
+//   تواصل عبر هذا البوت
+//   @Suggest_librarybot`;
+
+//   const ComplainMessage = `ما هي مشكلتك؟
+
+//   اكتب مشكلتك فى هذا البوت
+//   @Complain_fasilabot`;
+
+//   const PaymentMessage = `لتأكيد الدفع لازم تبعت البيانات دى كاملة:
+
+//   ١- سكرين شوت من رسالة التحويل (او صورة الايصال لو التحويل من ماكينة مثل فوري وامان ….)
+//   ٢- الرقم اللي تم التحويل منه (ضروري جدا)
+//   ٣- وقت التحويل
+//   ٤- الاسم كامل باللغة العربية (نفس الاسم فى الاكونت والفورم)
+//   ٥- الجامعة والسنة الدراسية (الفرقة)
+
+//   واللى مش هيبعت البيانات دى كاملة مش هنعتبر ده تأكيد للدفع
+
+//   🪧رجاءا فى رسالة واحدة☑️
+
+//   ارسل البيانات دي علي الاكونت دا👇
+//   @Fasila_Library`;
+
+//   let message;
+//   switch (reply) {
+//     case "payment":
+//       message = PaymentMessage;
+//       break;
+//     case "complain":
+//       message = ComplainMessage;
+//       break;
+//     case "suggest":
+//       message = SuggestMessage;
+//       break;
+//     case "publish":
+//       message = PublishMessage;
+//       break;
+//   }
+
+//   try {
+//     const sent = await bot.sendMessage(chatId, message);
+//     logMessageSent(sent);
+//   } catch (err) {
+//     logErrorMessage(err);
+//   }
+// };
 // //------------------------------------------//
 // function startTelegramBot() {
 //   bot.on("message", async (msg) => {
 //     if (msg.text === "/start") {
 //       await sendWelcomeMessage(msg);
 //     }
-//     // if (msg.text === "") {
-//     // }
+//     if (msg.text === "/payment") {
+//       await sendResponseMessage(msg.chat.id, "payment");
+//     }
+//     if (msg.text === "/complain") {
+//       await sendResponseMessage(msg.chat.id, "complain");
+//     }
+//     if (msg.text === "/suggest") {
+//       await sendResponseMessage(msg.chat.id, "suggest");
+//     }
+//     if (msg.text === "/publish") {
+//       await sendResponseMessage(msg.chat.id, "publish");
+//     }
 //   });
 // }
 // //------------------------------------------//
