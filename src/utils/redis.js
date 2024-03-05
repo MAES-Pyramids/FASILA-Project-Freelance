4e82cca8a8235bdcae827b642b596acbb1d54f24
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-03-05 02:29:29 +0200

    final commit

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -6,7 +6,7 @@ const redisPort = process.env.redisPort;
 const redis = new Redis({
   host: redisHost,
   port: redisPort,
-});
+}); // remember to edit host based on environment so if we are using docker container for both server and redis we will use my-redis-container name as host
 
 // const redis = new Redis(
 //   "redis://default:6e50bbc933ee4249b18aab3cb8793fd0@us1-robust-whale-41582.upstash.io:41582"
